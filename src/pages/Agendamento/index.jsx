ad9917d95ea94c0f91bed9e6bcce88300bb7134d
Guilherme Francisco <guilhermefranciscoav@gmail.com>
2021-04-20 16:36:51 -0300

    Update pages(Agendamento and ListaAgendamento)

--- src/pages/Agendamento/index.jsx
+++ src/pages/Agendamento/index.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import {
   Form, Formik, Field, ErrorMessage,
 } from 'formik';
 import { toast } from 'react-toastify';
 import * as Yup from 'yup';
+import intervalToDuration from 'date-fns/intervalToDuration';
+// import formatISO9075 from 'date-fns/formatISO9075';
 import Page from '../../components/Page';
 import DatePickerType from './dataPickerType';
 import { AppContext } from '../../AppContextProvider';
@@ -13,17 +15,37 @@ import axios from '../../utils/api';
 export default function index() {
   const [agendamentos, setAgendamentos] = useContext(AppContext);
 
-  const onSubmit = async (values) => {
+  const InitialFormLocalStorage = JSON.parse(localStorage.getItem('dados do agendamento'));
+  const initialState = {
+    nome: InitialFormLocalStorage?.nome || '',
+    dataNasc: InitialFormLocalStorage?.dataNasc || '',
+    dataHoraAgend: InitialFormLocalStorage?.dataHoraAgend || '',
+  };
+  const [form, setForm] = useState(initialState);
+
+  const isIdoso = (dataNascimento) => {
+    const idade = intervalToDuration({
+      start: new Date(dataNascimento),
+      end: Date.now(),
+    });
+    // console.log(idade);
+    if (idade.years < 60) { // admitindo que uma pessoa é considerada idosa com 60+ anos
+      return false;
+    }
+    return true;
+  };
+
+  const onSubmit = async (formValues) => {
     try {
       const response = await axios.post('/agendamento', {
-        ...values, idoso: false, atendido: false, conclusao: '',
+        ...formValues, idoso: isIdoso(formValues.dataNasc), atendido: false, conclusao: '',
       });
 
       setAgendamentos([...agendamentos, response.data.data]);
 
       toast.info('Agendamento criado!');
     } catch (e) {
-      toast.error(e.message);
+      toast.error(e.response.data.message);
     }
   };
   const validationSchema = Yup.object().shape({
@@ -31,57 +53,77 @@ export default function index() {
     dataNasc: Yup.date().required('O campo da data de nascimento é obrigatório'),
     dataHoraAgend: Yup.date().required('O campo da data e horário do agendamento é obrigatório'),
   });
+
+  const onChange = (event, v) => {
+    if (typeof (event) === 'string') {
+      const name = event;
+      const value = new Date(v);
+      setForm({ ...form, [name]: value });
+    } else {
+      const { target: { name, value } } = event;
+      setForm({ ...form, [name]: value });
+    }
+  };
+  useEffect(() => {
+    const formLS = JSON.parse(localStorage.getItem('dados do agendamento'));
+    // transformar o datahoraAgenda
+    if (!formLS) {
+      return;
+    }
+    if (form.nome === '' && form.dataNasc === '' && form.dataHoraAgend === '') {
+      setForm(formLS);
+    }
+  }, []);
+
+  useEffect(() => {
+    // transformar o datahoraAgenda
+    localStorage.setItem('dados do agendamento', JSON.stringify(form));
+  }, [form]);
   return (
     <Page title="Realizar Agendamento">
       <Formik
-        initialValues={{
-          nome: '',
-          dataNasc: '',
-          dataHoraAgend: '',
-        }}
+        initialValues={initialState}
         validationSchema={validationSchema}
         onSubmit={onSubmit}
         validateOnMount
       >
-        {(props) => {
-          const {
-            values,
-            setFieldValue,
-          } = props;
-          return (
-            <Form>
-              <div className="form-group">
-                <label htmlFor="nome" className="form-label">Digite seu nome:</label>
-                <Field name="nome" className="form-control" />
-                <ErrorMessage name="nome">
-                  { (msg) => <div style={{ color: 'red' }}>{msg}</div> }
-                </ErrorMessage>
-              </div>
-              <div className="form-group">
-                <label htmlFor="dataNasc" className="form-label">Digite sua data de nascimento:</label>
-                <Field name="dataNasc" type="date" className="form-control" />
-                <ErrorMessage name="dataNasc">
-                  { (msg) => <div style={{ color: 'red' }}>{msg}</div> }
-                </ErrorMessage>
-              </div>
-              <div className="form-group">
-                <label htmlFor="dataHoraAgend" className="form-label">Informe a data e horário do agendamento: </label>
-                <br />
-                <DatePickerType
-                  name="dataHoraAgend"
-                  value={values.dataHoraAgend}
-                  onChange={setFieldValue}
-                  className="form-control"
-                />
-                <br />
-                <ErrorMessage name="dataHoraAgend">
-                  { (msg) => <div style={{ color: 'red' }}>{msg}</div> }
-                </ErrorMessage>
-              </div>
-              <button className="btn btn-primary mt-2" type="submit">Efetuar Agendamento</button>
-            </Form>
-          );
-        }}
+        {(props) => (
+          <Form>
+            <div className="form-group">
+              <label htmlFor="nome" className="form-label">Digite seu nome:</label>
+              <Field name="nome" className="form-control" onChange={(e) => { props.handleChange(e); onChange(e); }} value={props.values.nome} />
+              <ErrorMessage name="nome">
+                { (msg) => <div style={{ color: 'red' }}>{msg}</div> }
+              </ErrorMessage>
+            </div>
+            <div className="form-group">
+              <label htmlFor="dataNasc" className="form-label">Digite sua data de nascimento:</label>
+              <Field name="dataNasc" type="date" className="form-control" onChange={(e) => { props.handleChange(e); onChange(e); }} value={props.values.dataNasc} />
+              <ErrorMessage name="dataNasc">
+                { (msg) => <div style={{ color: 'red' }}>{msg}</div> }
+              </ErrorMessage>
+            </div>
+            <div className="form-group">
+              <label htmlFor="dataHoraAgend" className="form-label">Informe a data e horário do agendamento: </label>
+              <br />
+              <DatePickerType
+                name="dataHoraAgend"
+                value={props.values.dataHoraAgend}
+                // onChange={(e, v) => {
+                //   props.setFieldValue();
+                //   onChange(e, v);
+                // }}
+                onChange={props.setFieldValue}
+                className="form-control"
+              />
+              <br />
+              <ErrorMessage name="dataHoraAgend">
+                { (msg) => <div style={{ color: 'red' }}>{msg}</div> }
+              </ErrorMessage>
+            </div>
+            <button className="btn btn-primary mt-2" type="submit">Efetuar Agendamento</button>
+          </Form>
+        )}
       </Formik>
     </Page>
   );
--- src/pages/ListaAgendamento/index.jsx
+++ src/pages/ListaAgendamento/index.jsx
@@ -4,6 +4,7 @@ import React, { useContext, useState } from 'react';
 import { Table, Form } from 'react-bootstrap';
 import format from 'date-fns/format';
 import formatISO9075 from 'date-fns/formatISO9075';
+// import getMinutes from 'date-fns/getMinutes';
 import { toast } from 'react-toastify';
 import Page from '../../components/Page';
 import { AppContext } from '../../AppContextProvider';
@@ -15,7 +16,7 @@ export default function ListaAgendamento() {
   const agendamentosSorted = [...agendamentos]; // ordenado por ordem de Agendamento
   const [conclusaoAgendamento, setConclusaoAgendamento] = useState({});
   const [showModal, setShowModal] = useState(false); //
-  const [text, setText] = useState(); //
+  const [text, setText] = useState(''); //
 
   function formatarHorario(data) {
     return format(new Date(data), 'HH:mm');
@@ -27,15 +28,15 @@ export default function ListaAgendamento() {
 
   agendamentosSorted.sort((a, b) => new Date(a.dataHoraAgend) - new Date(b.dataHoraAgend));
 
-  // console.log(agendamentosSorted);
-
   const grupos = agendamentosSorted.reduce((grupo, agendamento) => {
-    const dataAgendamento = formatISO9075(new Date(agendamento.dataHoraAgend), { representation: 'date' });
-    if (!grupo[dataAgendamento]) {
-      // eslint-disable-next-line no-param-reassign
-      grupo[dataAgendamento] = [];
+    if (agendamento.dataHoraAgend) {
+      const dataAgendamento = formatISO9075(new Date(agendamento.dataHoraAgend), { representation: 'date' });
+      if (!grupo[dataAgendamento]) {
+        // eslint-disable-next-line no-param-reassign
+        grupo[dataAgendamento] = [];
+      }
+      grupo[dataAgendamento].push(agendamento);
     }
-    grupo[dataAgendamento].push(agendamento);
     return grupo;
   }, {});
 
@@ -103,59 +104,64 @@ export default function ListaAgendamento() {
   };
   return (
     <Page title="Lista de Agendamentos">
-      { gruposAgendamentos.map((grupoAgendamentos, index) => (
-        <>
-          <Table key={index} bordered striped>
-            <thead>
-              <tr>
-                <th>
-                  Data e Horário do Agendamento (
-                  {formatarData(grupoAgendamentos.dataAgendamento)}
-                  )
-                </th>
-                <th width="33%">Nome</th>
-                <th>Área do enfermeiro</th>
-              </tr>
-            </thead>
-            <tbody>
-              {grupoAgendamentos.agendamento.map((agendamento, i) => (
-                <tr key={i}>
-                  <td>{formatarHorario(agendamento.dataHoraAgend)}</td>
-                  <td>{agendamento.nome}</td>
-                  <td>
-                    <label htmlFor="atendidoCheckbox">
-                      <input
-                        checked={agendamento.atendido}
-                        onChange={(event) => handleChecked(event, agendamento)}
-                        type="checkbox"
-                        name="atendidoCheckbox"
-                      />
-                      {' '}
-                      Atendido
-                    </label>
-                    <button className="btn btn-primary ml-2" type="button" onClick={() => handleConclusao(agendamento)}>
-                      {' Conclusão'}
-                    </button>
-                  </td>
+      <>
+        {gruposAgendamentos.length ? (
+          gruposAgendamentos.map((grupoAgendamentos, index) => (
+            <Table key={index} bordered striped>
+              <thead>
+                <tr>
+                  <th>
+                    Data e Horário do Agendamento (
+                    {formatarData(grupoAgendamentos.dataAgendamento)}
+                    )
+                  </th>
+                  <th width="33%">Nome</th>
+                  <th>Área do enfermeiro</th>
                 </tr>
-              ))}
-            </tbody>
-          </Table>
-          <Modal
-            onSubmit={onConclusaoAgendamento}
-            show={showModal}
-            toggle={() => handleConclusao()}
-            title={`Paciente: ${conclusaoAgendamento?.nome}`}
-          >
-            <Form>
-              <Form.Group>
-                <Form.Label>Conclusão do atendimento:</Form.Label>
-                <Form.Control value={text} onChange={({ target: { value } }) => setText(value)} />
-              </Form.Group>
-            </Form>
-          </Modal>
-        </>
-      ))}
+              </thead>
+              <tbody>
+                {grupoAgendamentos.agendamento.map((agendamento, i) => (
+                  <tr key={i}>
+                    <td>{formatarHorario(agendamento.dataHoraAgend)}</td>
+                    <td>{agendamento.nome}</td>
+                    <td>
+                      <label htmlFor="atendidoCheckbox">
+                        <input
+                          checked={agendamento.atendido}
+                          onChange={(event) => handleChecked(event, agendamento)}
+                          type="checkbox"
+                          name="atendidoCheckbox"
+                        />
+                        {' '}
+                        Atendido
+                      </label>
+                      <button className="btn btn-primary ml-2" type="button" onClick={() => handleConclusao(agendamento)}>
+                        {' Conclusão'}
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          ))) : (
+            <span className="empty-state">
+              Não há nenhum agendamento
+            </span>
+        )}
+        <Modal
+          onSubmit={onConclusaoAgendamento}
+          show={showModal}
+          toggle={() => handleConclusao()}
+          title={`Paciente: ${conclusaoAgendamento?.nome}`}
+        >
+          <Form>
+            <Form.Group>
+              <Form.Label>Conclusão do atendimento:</Form.Label>
+              <Form.Control value={text} onChange={({ target: { value } }) => setText(value)} />
+            </Form.Group>
+          </Form>
+        </Modal>
+      </>
     </Page>
   );
 }
